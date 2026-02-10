4101c7d712d8d0c1d1ab5b6ac835e3483cb79ef1
tsgsz <tsgsz@tsgsz-mac-mini.local>
2026-02-10 10:19:41 +0800

    refine

---
+++ downloads/code/thinking-in-design-of-blade-cpp-build/fool.cc
@@ -0,0 +1,16 @@
+// Copyright 2013, Kingslanding Inc.
+// Author: Pine <cdtsgsz@gmail.com>
+//
+// Description: fool implement file
+
+#include "test/fool.h"
+
+#include <stdio.h>
+
+namespace kingslanding {
+namespace test {
+    void Fool::Say() {
+       printf("haha, I'm a fool!!!");
+    }
+}  // namespace test
+}  // namespace kingslanding
